d109fb18fcf14aa8c0a694fa5ebb32ca360d34e9
Diego Bezerra Martins <diegobmartins21@gmail.com>
2020-05-15 09:33:23 -0300

    render props project started

---
+++ render-prop/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const App = () => <Amount />;
+
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>
+const Real = ({ amount }) => <p>Real: {amount * 7.11}</p>
+
+class Amount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: 57500,
+    };
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({amount: state.amount + 1 }));
+  };
+  onDecrement = () => {
+    this.setState(state => ({amount: state.amount - 1 }));
+  };
+
+  render () {
+    return (
+      <div>
+        <span>US Dollar: {this.state.amount} </span>
+        <button type="button" onClick={this.onIncrement}>
+          +
+        </button>
+        <button type="button" onClick={this.onDecrement}>
+          -
+        </button>
+        <Euro amount={this.state.amount} />
+        <Pound amount={this.state.amount} />
+        <Real amount={this.state.amount} />
+      </div>
+    );
+  }
+}
+
+export default App;
